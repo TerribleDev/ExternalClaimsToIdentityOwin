688ee2ab4a8825e76753a43cb8c94b4ad451f590
Tommy Parnell <tparnell8@gmail.com>
2016-10-03 15:43:03 -0400

    Add project files.

---
+++ GetWordpressOAuthToken/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GetWordpressOAuthToken.MvcApplication" Language="C#" %>
---
+++ GetWordpressOAuthToken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetWordpressOAuthToken.Startup))]
+namespace GetWordpressOAuthToken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
